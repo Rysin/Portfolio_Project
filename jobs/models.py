0402396a3795ce841a0e9636d7a2e57e55e90d40
rysin <sourabh301094@gmail.com>
2020-01-24 09:47:35 +0530

    Changed Image location for jobs and added home method to jobs.views

--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Job(models.Model):
 	# Store Images to DB
-	image = models.ImageField(upload_to='blog_images/')
+	image = models.ImageField(upload_to='job_images/')
 	summary = models.CharField(max_length=200)
