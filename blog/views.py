20a26e2ffbe10dc773608e4e8a08f0cf3495ad01
rysin <sourabh301094@gmail.com>
2020-01-28 14:00:14 +0530

    Added urls.py and views.py for allblogs.html without 'name' trick

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
+
 
 # Create your views here.
+def allblogs(request):
+	# Be Careful about the relative paths. /blog/ is terrorism
+	blogObjects = Blog.objects
+	return render(request, 'blog/allblogs.html', {'blogObjects': blogObjects})
