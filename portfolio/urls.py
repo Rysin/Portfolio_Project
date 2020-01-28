fff3d0349a4719358e9c1d96d7fb2b9bbe544873
rysin <sourabh301094@gmail.com>
2020-01-28 14:01:15 +0530

    Added path for blog urls using include function

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 import jobs.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', views.home)
-    path('', jobs.views.home)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  # path('', views.home),
+                  path('', jobs.views.home),
+                  path('blog/', include('blog.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
